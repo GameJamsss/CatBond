b16be3e050b011cf43cf83a071134d29c1999ae7
MsArgona <elenapichugina2012@gmail.com>
2022-11-05 00:29:14 +0400

    Added a logic on cursor click event

--- Assets/Scripts/UI/CursorController.cs
+++ Assets/Scripts/UI/CursorController.cs
@@ -1,44 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Scripts.Items;
 
-public class CursorController : MonoBehaviour
+namespace Assets.Scripts.UI
 {
-    [SerializeField] private float moveSpeed = 100f;
-    [SerializeField] private Sprite _idle;
-    [SerializeField] private Sprite _click;
-    [SerializeField] private float _clickTime = 0.6f;
-
-    private Vector3 mousePosition;
-    private SpriteRenderer _spriteRenderer;
-
-    private void Awake()
+    public class CursorController : MonoBehaviour
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-    }
+        [SerializeField] private float _moveSpeed = 100f;
+        [SerializeField] private Sprite _idle;
+        [SerializeField] private Sprite _click;
+        [SerializeField] private float _clickTime = 0.6f;
 
-    private void Start()
-    {
-        Cursor.visible = false;
-        _spriteRenderer.sprite = _idle;
-    }
+        [SerializeField] private float _checkCircleRadius = 1f;
 
-    void Update()
-    {
-        mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        private Vector3 mousePosition;
+        private SpriteRenderer _spriteRenderer;
 
-        if (Input.GetMouseButton(0))
+        private void Awake()
         {
-            StartCoroutine(ClickCoroutine());
+            _spriteRenderer = GetComponent<SpriteRenderer>();
         }
-    }
 
-    IEnumerator ClickCoroutine()
-    {
-        _spriteRenderer.sprite = _click;
-        yield return new WaitForSeconds(_clickTime);
-        _spriteRenderer.sprite = _idle;
+        private void Start()
+        {
+            Cursor.visible = false;
+            _spriteRenderer.sprite = _idle;
+        }
+
+        void Update()
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, _moveSpeed);
+
+            if (Input.GetMouseButton(0))
+            {
+                CheckItems();
+                StartCoroutine(ClickCoroutine());
+            }
+        }
+
+        IEnumerator ClickCoroutine()
+        {
+            _spriteRenderer.sprite = _click;
+            yield return new WaitForSeconds(_clickTime);
+            _spriteRenderer.sprite = _idle;
+        }
+
+        private void CheckItems()
+        {
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, _checkCircleRadius);
+
+            foreach (var collider in colliders)
+            {
+                if (collider.CompareTag("Item"))
+                {
+                    Debug.Log("I click on item"); //calls few times in a row i dunno why
+
+                    //if (collider.GetComponent<DecorativeItem>() != null)
+                    //    collider.GetComponent<DecorativeItem>().Click();
+
+                    //if (collider.GetComponent<Item>() != null)
+                    //    collider.GetComponent<Item>().Click();
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, _checkCircleRadius);
+        }
     }
 }
