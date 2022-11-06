db3d70f831c13a4e513278ed60ed95a94ce8fe70
S0u1 <shestaevd26@gmail.com>
2022-11-06 12:37:23 +0400

    jopa

--- Assets/Scripts/Domain/Interfaces/ItemsId.cs
+++ Assets/Scripts/Domain/Interfaces/ItemsId.cs
@@ -1,12 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.UI
 {
-    public enum ItemsId
+    public static class ItemsId
     {
-        Cup,
-        CoffeeJar
+        public static Tuple<int, string> Cup = new(1, "");
+        public static Tuple<int, string> CoffeeBeans = new(2, "");
+        public static Tuple<int, string> Coin = new(3, "");
+        public static Tuple<int, string> PlasticBottle = new(4, "");
+        public static Tuple<int, string> CoffeeCup = new(5, "");
+
+        public static Dictionary<int, string> AllItems = new()
+        {
+            [Cup.Item1] = Cup.Item2,
+            [CoffeeBeans.Item1] = CoffeeBeans.Item2,
+            [Coin.Item1] = Coin.Item2,
+            [PlasticBottle.Item1] = PlasticBottle.Item2,
+            [CoffeeCup.Item1] = CoffeeCup.Item2,
+        };
     }
 }
