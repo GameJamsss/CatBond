671613c05e483cdb1fd134eca9a4f921b13baed9
MsArgona <elenapichugina2012@gmail.com>
2022-11-06 17:03:32 +0400

    Added new objects

--- Assets/Scripts/Managers/CheckListManager.cs
+++ Assets/Scripts/Managers/CheckListManager.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Assets.Scripts.Domain.State;
 
 namespace Assets.Scripts.Managers
 {
     public class CheckListManager : MonoBehaviour
     {
         [SerializeField] private Toggle[] _toggles;
+        [SerializeField] private GameObject _boss;
+
+        private int _marktedItemsCount = 0;
 
         public void MarkItem(int id)
         {
             _toggles[id].isOn = true;
+            _marktedItemsCount++;
+
+            if (_marktedItemsCount == _toggles.Length)
+            {
+                _boss.GetComponent<StateManager>()._currentStateId = 1;
+            }
         }
     }
 }
