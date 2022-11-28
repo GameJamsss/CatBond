c526c1473eeff1ec26ee51b137b55eb541505033
MsArgona <elenapichugina2012@gmail.com>
2022-11-29 02:02:37 +0400

    Expanded Dog Custom Logic

--- Assets/Scripts/Domain/Objects/ContextMenuButtons/CustomDogLogic.cs
+++ Assets/Scripts/Domain/Objects/ContextMenuButtons/CustomDogLogic.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 using Assets.Scripts.Domain.Objects.ContextMenuButtons.CustomLogic;
 using Assets.Scripts.Managers;
 using Assets.Scripts.Domain;
+using Assets.Scripts.Utils;
+using CSharpFunctionalExtensions;
 
 public class CustomDogLogic : MonoBehaviour, ICustomLogic
 {
     [SerializeField] private int _id;
     DialogManager _dialogManager;
-
-    private void Start()
-    {
-        _dialogManager = FindObjectOfType<DialogManager>();
-    }
 
     public void Apply()
     {
@@ -22,9 +19,13 @@ public class CustomDogLogic : MonoBehaviour, ICustomLogic
         lines.Enqueue(new Line("Босс: О, список это замечательно! Но есть один нюанс... Теперь все это нужно собрать и упаковать в коробки до конца рабочего дня."));
         lines.Enqueue(new Line("К: ЧТО?!"));
         lines.Enqueue(new Line("Б: У вас 15 минут. Советую поторопиться!"));
-        FindObjectOfType<DialogManager>().StartDialog(new Dialog(lines));
 
-        //Заершение игры
+        MaybeRich.NullSafe(FindObjectOfType<DialogManager>()).Match(dem => dem.StartDialog(new Dialog(lines, TheEndOfEverything)), () => Debug.Log("DialogManager doesn't found")); 
+    }
+
+    private void TheEndOfEverything()
+    {
+        //Завершение игры. Проиграть заставку
     }
 
     public int GetId()
